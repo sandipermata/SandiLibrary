4cc20e691132d51e0c091b12e9e981a9bb3fedbd
sandi <sandy.spyware@gmail.com>
2022-06-24 05:59:36 +0700

    add sampleUsingLibrary

---
+++ SampleUsingLibrary/Pods/Target Support Files/SandiLibrary/SandiLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SandiLibrary : NSObject
+@end
+@implementation PodsDummy_SandiLibrary
+@end
---
+++ SampleUsingLibrary/SampleUsingLibrary/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SampleUsingLibrary
+//
+//  Created by SANDI on 23/06/22.
+//
+
+import UIKit
+import SandiLibrary
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let data = dataString()
+        data.checkData()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
