6fa7ee99489ef7120d71aac0f318a1ca2fbe46ef
sandi <sandy.spyware@gmail.com>
2022-06-23 07:47:34 +0700

    add function

---
+++ SandiLibrary/SandiLibrary/dataString.swift
@@ -0,0 +1,17 @@
+//
+//  dataString.swift
+//  SandiLibrary
+//
+//  Created by SANDI on 23/06/22.
+//
+
+import Foundation
+
+public class dataString {
+    
+    public init() {}
+    
+    public func checkData() {
+        print("call function data string")
+    }
+}
