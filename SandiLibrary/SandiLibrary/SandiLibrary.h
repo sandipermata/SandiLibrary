b607131006557c15bba147659de0ec1fa2330e4b
sandi <sandy.spyware@gmail.com>
2022-06-23 07:34:23 +0700

    add project xcode

---
+++ SandiLibrary/SandiLibrary/SandiLibrary.h
@@ -0,0 +1,18 @@
+//
+//  SandiLibrary.h
+//  SandiLibrary
+//
+//  Created by SANDI on 23/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SandiLibrary.
+FOUNDATION_EXPORT double SandiLibraryVersionNumber;
+
+//! Project version string for SandiLibrary.
+FOUNDATION_EXPORT const unsigned char SandiLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SandiLibrary/PublicHeader.h>
+
+
